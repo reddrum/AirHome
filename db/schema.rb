34f8f998f4fded38f0d328ac3a76c649a6783f45
“reddrum” <anton.kranz@me.com>
2018-08-26 11:05:06 +0300

    Added user_profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_150930) do
+ActiveRecord::Schema.define(version: 2018_08_26_075314) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2018_08_25_150930) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "phone_number"
+    t.text "description"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
