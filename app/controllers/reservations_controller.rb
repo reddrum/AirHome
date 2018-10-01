0081f956d64b3f8b2904e9c79a97de57225b7dfc
“reddrum” <anton.kranz@me.com>
2018-10-01 23:51:44 +0300

    Added booking function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_reservation, only: [:approve, :decline]
 
   def create
     room = Room.find(params[:room_id])
@@ -14,10 +15,24 @@ class ReservationsController < ApplicationController
       @reservation = current_user.reservations.build(reservation_params)
       @reservation.room = room
       @reservation.price = room.price
-      @reservation.total = room.price * days
-      @reservation.save
+      # @reservation.total = room.price * days
+      # @reservation.save
 
-      flash[:notice] = "Booked Successfully!"
+      @reservation.total = room.price * (days - special_dates.count)
+      special_dates.each do |date|
+        @reservation.total += date.price
+      end
+
+      if @reservation.Waiting!
+        if room.Request?
+          flash[:notice] = "Request sent successfully!"
+        else
+          charge(room, @reservation)
+        end
+      else
+        flash[:alert] = "Cannot make a reservation!"
+      end
+
     end
     redirect_to room
   end
@@ -29,8 +44,23 @@ class ReservationsController < ApplicationController
   def your_reservations
     @rooms = current_user.rooms
   end
+
+  def approve
+    charge(@reservation.room, @reservation)
+    redirect_to your_reservations_path
+  end
+
+  def decline
+    @reservation.Declined!
+    redirect_to your_reservations_path
+  end
   
   private
+
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
+
     def reservation_params
       params.require(:reservation).permit(:start_date, :end_date)
     end
