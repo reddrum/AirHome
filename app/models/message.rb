4d97e1248b46ce7f06f331960fb79670155bf9f0
“reddrum” <anton.kranz@me.com>
2018-10-14 22:40:23 +0300

    Added conversations

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :conversation
+
+  validates_presence_of :context, :conversation_id, :user_id
+  after_create_commit :create_notification
+
+  def message_time
+    self.created_at.strftime("%v")
+  end
+end
